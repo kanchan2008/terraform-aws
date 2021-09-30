74f5ca803f38e04712b61c1821ea0a4839913fcd
kanchan2008 <jgec007@gmail.com>
2021-09-30 18:40:37 +0530

    correction

--- terraform-aws/Backend/create-remote-storage.tf
+++ terraform-aws/Backend/create-remote-storage.tf
@@ -9,10 +9,10 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id   = ${env.ARM_SUBSCRIPTION_ID}
-  tenant_id         = ${env:ARM_TENANT_ID}
-  client_id         = ${env:ARM_CLIENT_ID}
-  client_secret     = ${env:ARM_CLIENT_SECRET}
+  subscription_id   = "${env.ARM_SUBSCRIPTION_ID}"
+  tenant_id         = "${env:ARM_TENANT_ID}"
+  client_id         = "${env:ARM_CLIENT_ID}"
+  client_secret     = "${env:ARM_CLIENT_SECRET}"
 }
 
 resource "random_string" "resource_code" {
