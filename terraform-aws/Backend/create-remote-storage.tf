78a3a42b70c8b9886d1794225ea86b79b3c356c0
kanchan2008 <jgec007@gmail.com>
2021-10-01 02:09:26 +0530

    Update create-remote-storage.tf

--- terraform-aws/Backend/create-remote-storage.tf
+++ terraform-aws/Backend/create-remote-storage.tf
@@ -5,42 +5,20 @@ terraform {
       version = "=2.46.0"
     }
   }
+    backend "azurerm" {
+        resource_group_name  = "tfstate"
+        storage_account_name = "tfstate410197371"
+        container_name       = "tfstate"
+        key                  = "terraform.tfstate"
+    }
+
 }
 
 provider "azurerm" {
   features {}
-  subscription_id   = "${env.ARM_SUBSCRIPTION_ID}"
-  tenant_id         = "${env:ARM_TENANT_ID}"
-  client_id         = "${env:ARM_CLIENT_ID}"
-  client_secret     = "${env:ARM_CLIENT_SECRET}"
 }
 
-resource "random_string" "resource_code" {
-  length  = 5
-  special = false
-  upper   = false
-}
-
-resource "azurerm_resource_group" "tfstate" {
-  name     = "tfstate"
-  location = "East US"
-}
-
-resource "azurerm_storage_account" "tfstate" {
-  name                     = "tfstate${random_string.resource_code.result}"
-  resource_group_name      = azurerm_resource_group.tfstate.name
-  location                 = azurerm_resource_group.tfstate.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-  allow_blob_public_access = true
-
-  tags = {
-    environment = "staging"
-  }
-}
-
-resource "azurerm_storage_container" "tfstate" {
-  name                  = "tfstate"
-  storage_account_name  = azurerm_storage_account.tfstate.name
-  container_access_type = "blob"
+resource "azurerm_resource_group" "state-demo-secure" {
+  name     = "state-demo"
+  location = "eastus"
 }
